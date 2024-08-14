5b580ea01662e4244ad1ba0af4142a27a74b521b
deygobinda <deygobinda2808@gmail.com>
2024-08-14 10:44:21 +0530

    responsiveness-added

--- frontend/src/components/FullBlog.tsx
+++ frontend/src/components/FullBlog.tsx
@@ -7,16 +7,16 @@ export const FullBlog = ({ blog }: { blog: Blog }) => {
     <div className="h-screen">
       <Appbar />
       <div className="flex justify-center  pt-32">
-        <div className="grid grid-cols-12 px-10 w-full  max-w-screen-2xl">
+        <div className="grid lg:grid-cols-12 px-10 w-full  max-w-screen-2xl">
           <div className="col-span-8">
-            <div className="text-5xl font-extrabold">{blog.title}</div>
+            <div className="text-3xl lg:text-5xl font-extrabold">{blog.title}</div>
             <div className="text-slate-500 pt-4">
               {blog.date.slice(0,10)}
             </div>
             <div className="pt-4">{blog.content}</div>
           </div>
           <div className="col-span-4">
-            <div className="pl-20">
+            <div className="lg:pl-20">
               <div className="text-slate-600">Author</div>
               <div className="flex gap-4 items-center pt-4">
                 <div>
